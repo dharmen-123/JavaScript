f1fffa90a8bcb12036bc911f6c6d63bee310d920
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-02-24 16:15:08 +0530

    newchange

---
+++ 10daysJavaScript/cssapply.js
@@ -0,0 +1,18 @@
+let ntag =document.createElement('h1')
+ntag.textContent="Create Element is used create Element in HTML through JS"
+let a =document.body
+a.append(ntag)
+
+let tag2 = document.createElement('h2')
+tag2.textContent='Welldone'
+//  let b=document.body
+ a.append(tag2)
+
+let img=document.createElement('img')
+img.src='car bunglow copy.jpg'
+img.style.width='400px'
+img.style.borderRadius="15px"
+img.style.padding="4px"
+img.style.margin="4%"
+img.style.boxShadow="5px 5px 15px 8px black"
+a.append(img)
