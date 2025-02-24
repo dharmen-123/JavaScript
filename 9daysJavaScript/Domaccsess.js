a9dacd31e8c72819e2e354f5a679282a06e794c0
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-02-24 10:52:22 +0530

    changes

--- 9daysJavaScript/Domaccsess.js
+++ 9daysJavaScript/Domaccsess.js
@@ -1,13 +1,18 @@
 const a=document.getElementById('change')
 a.src="car bunglow copy.jpg"
 a.style.borderRadius="15px"
-a.style.boxShadow="5px 5px 12px 8px black"
+a.style.padding="4px"
+a.style.margin="4%"
+a.style.boxShadow="5px 5px 15px 8px black"
 
 let para = document.getElementById('para')
 para.innerHTML="<b> This is new changed text</b>"
-// par.style.fontweight ="bold"
-// par.style.fontsize ="30px"
+para.style.fontSize ="30px"
 
 let buttn = document.getElementById('butt')
-buttn.style.backgroundColor="lightblue"
+buttn.style.backgroundColor="blue"
 buttn.style.padding="15px"
+buttn.style.fontSize="20px"
+buttn.style.fontWeight ="bold"
+buttn.style.color="white"
+buttn.style.borderRadius="15px"
