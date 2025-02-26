dfb7f46974e46ec5c1baa04b6ab64efade04287d
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-02-26 16:39:54 +0530

    chnage

--- 12daysJavaScript/Events.js
+++ 12daysJavaScript/Events.js
@@ -1,11 +1,27 @@
-let h=0
-function plus(){
-    let head=document.getElementById('head')
-    head.innerHTML = h++ 
+// let h=0
+// function plus(){
+//     let head=document.getElementById('head')
+//     head.innerHTML = h++ 
+// }
+// function minus(){
+//     if(h>0){
+//     head.innerHTML = --h
+// }
+// }
+function App(){
+let t1=document.getElementById('change')
+    t1.src="apple.jpeg"
 }
-function minus(){
+function Ora(){
+    t1.src="orange.jpeg"
+}
+function Wat(){
+    t1.src="watermelon.jpeg"
+}
+function Pin(){
+    t1.src="12daysJavaScript\pineapple.jpeg"
+}
+function Sap(){
+    t1.src="sapodila.jpeg"
+}
 
-    if(h>0){
-    head.innerHTML = --h
-}
-}
