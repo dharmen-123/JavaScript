5a2f4ad1e545a56a01b62af32d195f1a760095a0
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-02-28 16:20:38 +0530

    commit

--- 13daysJavaScript/targetclass.js
+++ 13daysJavaScript/targetclass.js
@@ -1,4 +1,41 @@
-let head = document.querySelector('.hello')
-head.textContent="hello"
-let hid =document.querySelector('#head')
-hid.style.color="red"
+// let head = document.querySelector('.hello')
+// head.textContent="hello"
+// let hid =document.querySelector('#head')
+// hid.style.color="red"
+// let c=document.querySelector('h2')
+// c.style.color="blue"
+// let list = document.querySelectorAll('li')
+// list[2].style.color="red"
+
+
+  //Q.1 
+let nam ="Dharmendra Chilhate"
+console.log(nam);
+
+//Q.2
+let a =10
+let b=12
+let c=a
+   a=b
+   b=c
+document.write("The value of a:"+ a +"<br>"+" The value of b:"+ b+"<br>")
+
+//Q.3
+// let n=parseInt(prompt("Enter the number"))
+// if(n%2==0){
+//     document.write("This is even number")
+// }
+// else{
+//     document.write("this is odd number")
+// }
+
+//Q.4
+let age =parseInt(prompt("Enter the number"))
+if(age>=18){
+    document.write("you are eligible for vote")
+}
+else{
+    document.write("not eligible")
+}
+
+
