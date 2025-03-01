d9d7fdd67e8e3a519eff4ae2824d5e995019253f
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-01 16:03:54 +0530

    hello

--- 14daysJavaScript/Arrowfunc.js
+++ 14daysJavaScript/Arrowfunc.js
@@ -1,4 +1,10 @@
-let a=()=>{return "dharmendra"}
-console.log(a());
-let b=(c,d)=>{return c+d}
-console.log(b(3,3));
+// let a=()=>{return "dharmendra"}
+// console.log(a());
+// let b=(c,d)=>{return c+d}
+// console.log(b(3,3));
+
+//WAP to take one input from the user anf find area of circle using arrow function with parameter
+
+let fun=(r)=>{return  3.14*r*r}
+let val=parseInt(prompt("enter the radius value"))
+console.log(fun(val))
