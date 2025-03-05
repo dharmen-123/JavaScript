548b2b60e7482d98af0226b8fe1f231f99bd1f22
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-05 16:31:09 +0530

    change

---
+++ 17daysJavaScript/settimeout.js
@@ -0,0 +1,16 @@
+
+// setTimeout(()=>{alert("hello")},3000)
+
+// let head =document.getElementById('head')
+// setTimeout(()=>{head.style.display="block"},5000)
+
+function fun(){
+
+    let num1=parseInt(document.querySelector('#num1').value)
+    let num2 =parseInt(document.querySelector('#num2').value)
+    let a= (num1+num2)
+    let head=document.getElementById('head') 
+    head.innerText= a
+    return false
+}
+// setTimeout(()=>{fun()},2000)
