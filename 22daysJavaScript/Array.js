bdf9f2098abb7ce38f0058f9a076776a76a56b58
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-10 16:31:05 +0530

    commit

--- 22daysJavaScript/Array.js
+++ 22daysJavaScript/Array.js
@@ -4,10 +4,16 @@
     // for of loop is used for print the array values 
     // for in loop is used for print the indexing of array elements
 
-let ar1=[12,23.4,'D',"JavaScript",true]
-for(let vl of ar1)
+// let ar1=[12,23.4,'D',"JavaScript",true]
+// for(let vl of ar1)
+// console.log(vl);
+
+// for(let k in ar1)
+// console.log(k);
+
+
+let ar2=[]
+for(let i=0 ;i<5;i++){
+vl[i]=parseInt(prompt("Enter the values"))
+}
 console.log(vl);
-
-for(let k in ar1)
-console.log(k);
-
