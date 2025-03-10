bc4bdaf00108614187211fb6eaa969b58a5d782a
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-10 16:21:44 +0530

    update

---
+++ 22daysJavaScript/Array.js
@@ -0,0 +1,9 @@
+    //  Array is collection of different type of data 
+    //  [],ordered 
+    // index is position of array or address of array
+    // for of loop is used for print the array values 
+    // for in loop is used for print the indexing of array elements
+
+let ar1=[12,23.4,'D',"JavaScript",true]
+for(let vl of ar1)
+console.log(vl);
