acca0231939acf92eb66bc8a5d842f633ff379ef
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-12 16:02:57 +0530

    commit

--- 24daysJavaScript/arrayoperation.js
+++ 24daysJavaScript/arrayoperation.js
@@ -1,8 +1,9 @@
 let arr1 = [2,34,38.5,'h']
 arr1.push("hello","javaScript")
-// console.log(arr1);
-// arr1.pop('2')
 arr1.unshift("first","second" ,"third")
 console.log(arr1);
+arr1.pop()
+console.log(arr1);
+
 
 
