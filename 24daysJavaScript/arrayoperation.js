07ece5d0da742b2d2b92c7f01268e713e74d7163
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-18 18:18:52 +0530

    commit

--- 24daysJavaScript/arrayoperation.js
+++ 24daysJavaScript/arrayoperation.js
@@ -2,8 +2,9 @@
 // arr1.push("hello","javaScript")
 // arr1.unshift("first","second" ,"third")
 // console.log(arr1);
-// // poop function is used for remove the last elements from the array 
-// //shift funtion is used for remove the elements from first position of array
+
+ // poop function is used for remove the last elements from the array 
+ //shift funtion is used for remove the elements from first position of array
 
 // arr1.pop()
 // console.log(arr1);
