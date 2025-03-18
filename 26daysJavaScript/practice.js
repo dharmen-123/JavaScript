7efe055b9f20cd0966c35b6d4d9b1b8490fd1ce7
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-18 18:14:42 +0530

    new update

--- 26daysJavaScript/practice.js
+++ 26daysJavaScript/practice.js
@@ -1,5 +1,6 @@
 let inp=document.getElementById('inp'),
    box = document.getElementById('box');
+inp.setAttribute("style","width:500px; height:50px;")
 box.setAttribute("style","width:200px; height:200px; border:2px solid black;")
 inp.addEventListener("input",()=>{
   box.style.backgroundColor = inp.value
