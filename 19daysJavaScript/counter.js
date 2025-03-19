f55c61f05deaa5ead9c32bd4b173999c8ed62e39
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-19 19:14:32 +0530

    commit

--- 19daysJavaScript/counter.js
+++ 19daysJavaScript/counter.js
@@ -1,25 +1,25 @@
 let mdiv = document.querySelector('#maindiv')
-// mdiv.style.width="400px"
-// mdiv.style.height="400px"
-mdiv.setAttribute('style','width:200px; height:200px; border-radius:30px;text-align:center; border: 2px solid black; align-items:center; ')
+mdiv.setAttribute('style','width:250px; height:250px; border-radius:30px;text-align:center; border: 2px solid black; align-items:center; background-color:cyan ;')
 
+const body= document.querySelector('body')
+body.setAttribute("style","background-color:grey; display:flex; justify-content:center; align-items:center; height:100vh;")
+const text=document.querySelector('.text')
+text.setAttribute("style","text-align:center; font-size:50px;color:red;")
 let h=0
+
+let head=document.getElementById('head')
+head.setAttribute("style","font-size: 90px; margin-top:0%;")
+
 let onplus=document.querySelector('#onplus')
+onplus.setAttribute('style','font-size:30px; border-radius:10px;padding:10px; ')
 onplus.addEventListener('click',()=>{
-    head.innerHTML = h++ 
-         
+    head.innerHTML = h++        
 })
-onplus.setAttribute('style','font-size:35px; border-radius:10px;padding:10px; ')
 
 let onminus=document.querySelector('#onminus')
-onminus.setAttribute('style','font-size:35px; border-radius:10px;padding:10px; ')
-
-function plus(){
-    let head=document.getElementById('head')
-    head.innerHTML = h++ 
-}
-function minus(){
+onminus.setAttribute('style','font-size:30px; border-radius:10px;padding:10px; ')
+onminus.addEventListener('click',()=>{
     if(h>0){
-    head.innerHTML = --h
-}
-}
+        head.innerHTML = --h
+    }
+})
