8dc49276c58cffdd8177e048c94716e81ede1478
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-20 16:25:34 +0530

    commit

--- 27daysJavaScript/object.js
+++ 27daysJavaScript/object.js
@@ -1,12 +1,32 @@
-let stu={
-    stu_name:"Dhamendra",
-    stu_age:20,
-    stu_contact:1234,
-    stu_branch:"AIML",
-    stu_account:{
-        balance:200000,
-        acc_num:8765799
-    }
+// let stu={
+//     stu_name:"Dhamendra",
+//     stu_age:20,
+//     stu_contact:1234,
+//     stu_branch:"AIML",
+//     stu_account:{
+//         balance:200000,
+//         acc_num:8765799
+//     }
+// }
+// console.log(stu.stu_name,stu.stu_branch);
+// console.log(stu.stu_account.balance);
+
+
+function data(){
+let name=document.querySelector('#name').value
+let age= document.querySelector('#age').value
+let contact= document.querySelector('#contact').value
+let person ={
+    name: name ,
+    age: age,
+    contact: contact  
 }
-console.log(stu.stu_name,stu.stu_branch);
-console.log(stu.stu_account.balance);
+console.log(person);
+return false
+}
+
+
+
+
+
+
