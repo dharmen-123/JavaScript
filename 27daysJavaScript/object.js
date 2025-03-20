0bd06bcc8c0e8ed2b19cd8098568de6742d8a8b9
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-20 16:01:10 +0530

    commit

--- 27daysJavaScript/object.js
+++ 27daysJavaScript/object.js
@@ -2,6 +2,10 @@ let stu={
     stu_name:"Dhamendra",
     stu_age:20,
     stu_contact:1234,
-    stu_branch:"AIML"
+    stu_branch:"AIML",
+    stu_account:{
+        balance:20000,
+        acc_num:8765799
+    }
 }
-console.log(stu.stu_name);
+console.log(stu.stu_account.acc_num);
