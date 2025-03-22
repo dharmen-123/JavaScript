60c9cd452fc4a8af5bb276b87a6171aa1a2da5d0
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-22 15:51:00 +0530

    commit

---
+++ 29DaysJavaScript/obOperation.js
@@ -0,0 +1,9 @@
+let obj ={
+    name:"Dharmendra",
+    contact:10879,
+    fun:function(){
+        alert("OBJECT FUNCTION IS WORKING")
+    },
+    car:["BMW","MERCEDEZS","AUDI","ROLLS ROYCE"]
+}
+obj.fun()
