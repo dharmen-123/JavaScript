88efc7a55734d8a07e0546264c4ed0c45b9a9552
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-23 15:56:37 +0530

    commit

--- 28testjavaScript/test4.js
+++ 28testjavaScript/test4.js
@@ -1,48 +1,48 @@
     //    TASK 1 START FROM HERE
    
-// let hrs =document.querySelector('#hrs')
-// let min =document.querySelector('#min')
-// let sec =document.querySelector('#sec')
+let hrs =document.querySelector('#hrs')
+let min =document.querySelector('#min')
+let sec =document.querySelector('#sec')
 
-// let stp;
+let stp;
 
-// function start(){
-//     let [h,m,s]=[0,0,0]
-//     stp=setInterval(()=>{
-//         sec.innerHTML=s++;
-//         if(s==60){
-//             s=0;
-//             min.innerHTML=m++;
-//             if(m==60){
-//                 m=0;
-//                 hrs.innerHTML=h++;
-//             }
-//         }
-//     },10)
-// }
-// function stop(){
-//         clearInterval(stp)
-// }
-// function reset(){
-//     hrs.innerHTML="00"
-//     min.innerHTML="00"
-//     sec.innerHTML="00"
-// }
+function start(){
+    let [h,m,s]=[0,0,0]
+    stp=setInterval(()=>{
+        sec.innerHTML=s++;
+        if(s==60){
+            s=0;
+            min.innerHTML=m++;
+            if(m==60){
+                m=0;
+                hrs.innerHTML=h++;
+            }
+        }
+    },10)
+}
+function stop(){
+        clearInterval(stp)
+}
+function reset(){
+    hrs.innerHTML="00"
+    min.innerHTML="00"
+    sec.innerHTML="00"
+}
 
 
     //    TASK 2 START FROM HERE
 
-// let count=document.querySelector('#count')
-// let c=0;
+let count=document.querySelector('#count')
+let c=0;
 
-// function plus(){
-//    count.innerHTML = ++c;
-// }
-// function min(){
-//      if(c>0){
-//         count.innerHTML= --c;
-//      }
-// }
+function plus(){
+   count.innerHTML = ++c;
+}
+function min(){
+     if(c>0){
+        count.innerHTML= --c;
+     }
+}
 
 //  TASK 3 START FROM HERE
 
@@ -66,12 +66,12 @@ function day(){
 
 // TASK 4 START FROM HERE
 
-// let ar1=[1,2,3,4,5]
-// let ar2=[6,7,8,9,10]
-// let arm=[...ar1,...ar2]
-// // console.log(arm);
-// for(let i=0;i<10;i++){
-//     if(arm[i]%2==0){
-//         console.log(arm[i]);
-//     }
-// }
+let ar1=[1,2,3,4,5]
+let ar2=[6,7,8,9,10]
+let arm=[...ar1,...ar2]
+// console.log(arm);
+for(let i=0;i<10;i++){
+    if(arm[i]%2==0){
+        console.log(arm[i]);
+    }
+}
