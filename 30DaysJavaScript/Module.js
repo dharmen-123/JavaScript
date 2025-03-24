9c9c49d6c5b74ab27044fbfa694b7e58e4788261
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-24 16:20:39 +0530

    change

--- 30DaysJavaScript/Module.js
+++ 30DaysJavaScript/Module.js
@@ -2,16 +2,25 @@
 // Module is the library which is used is when it is require 
 // TYPES of MODULE 
 //  Import and Export 
-// It has also two types 1.Default 2.Named
+// It has also two types
+//  1.Default it is use as default keyword
+//  2.Named it is used as {function name}
 // Default is used when only one fuction is used
 // when any file have many function then used named import and export
 
 
 // import add from './func.js'
-import {add, sub} from './func.js'
-let ans= add(10,5)
+
+// By using Named import export
+
+// import {add, sub} from './func.js'
+// By change the name os function 
+
+import {add as Sum , sub as Minus} from './func.js'
+
+let ans= Sum(10,5)
 console.log(ans);
 
-let ans2 = sub(5,8)
+let ans2 = Minus(5,8)
 console.log(ans2);
 
