daaf25c9e75f63634c678b1a029576ebfce43c3f
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-27 15:55:39 +0530

    changes

---
+++ 33daysJavaScript/app.js
@@ -0,0 +1,7 @@
+
+// template literal is used to write the html with jS ` `
+
+// when the valriable is insert in the template literals use ${valriable name}
+let name = "Dhamendra"
+let text = `my name is ${name}`
+console.log(text);
