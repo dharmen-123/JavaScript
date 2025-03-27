07bda76216dd51f17f5d01bdc371b6c4b91043a8
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-27 15:58:11 +0530

    commit

--- 33daysJavaScript/app.js
+++ 33daysJavaScript/app.js
@@ -2,6 +2,8 @@
 // template literal is used to write the html with jS ` `
 
 // when the valriable is insert in the template literals use ${valriable name}
+// variable injection 
+
 let name = "Dhamendra"
 let text = `my name is ${name}`
 console.log(text);
