f09f67589c86ef73797220c8e710f38d8e37b3fd
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-02 18:28:27 +0530

    commit

--- 2daysJavaScript/2code.js
+++ 2daysJavaScript/2code.js
@@ -1,18 +1,20 @@
 //Q.1 Write a program to add two numbers and find the square of result
 
-let a=parseInt(prompt("Enter num 1:  "))
-let b=parseInt(prompt("Enter num 2:  "))
-var result=a+b
-document.write(result,"\n")
-var sqr=result*result 
-document.write("Square of result:",sqr,"    \n")
-//Q.2  Write a program to find the area of a circle using the formula A = πr2 where A
-let r = parseInt(prompt("Enter the radius : "))
-var cir=3.14*r*r
-document.write("Area of circle:",cir)
-//Q.3 Write a program to swap to value using third variables value should be taken from the user
+var a=parseInt(prompt("Enter num 1:  "))
+var b=parseInt(prompt("Enter num 2:  "))
+// var result=a+b
+// document.write(result,"\n")
+// var sqr=result*result 
+// document.write("Square of result:",sqr,"    \n")
+// //Q.2  Write a program to find the area of a circle using the formula A = πr2 where A
+// let r = parseInt(prompt("Enter the radius : "))
+// var cir=3.14*r*r
+// document.write("Area of circle:",cir)
+// //Q.3 Write a program to swap to value using third variables value should be taken from the user
 
-var temp=a
-    a=b
-    b=temp
-console.log("value of a:",a,"value of b:", b)
+// var temp=a
+//     a=b
+//     b=temp
+// console.log("value of a:",a,"value of b:", b)
+[a,b]=[b,a]
+console.log(a,b);
