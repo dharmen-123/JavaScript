3952be2c23f8b30ba93c39e28b03b7f4963e3cb4
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-03 16:26:29 +0530

    commit

--- 36daysjavaScript/fetctdata.js
+++ 36daysjavaScript/fetctdata.js
@@ -1,4 +1,3 @@
-
 let fetchData=async()=>{
      
     let url="http://localhost:3000/Hotel"
@@ -7,5 +6,58 @@ let fetchData=async()=>{
     let data=await resp.json()
     console.log(data);
     
+    let tabledata=document.querySelector('#tabledata')
+
+    data.map((e)=>{
+       
+        tabledata.innerHTML+=`
+            <tr>
+              <td>${e.name} </td>
+              <td>${e.age} </td>
+              <td>${e.addhar} </td>
+              <td>${e.Time} </td>
+              <td>${e.person} </td>
+            </tr>
+        
+        
+        `
+
+    })
+
+
 }
 fetchData()
+// let Ardata=[
+//     {
+//         "name":"dharmendra",
+//        "age":20,
+//        "addhar":30284,
+//        "Time":"10:20",
+//        "person":5
+//     },
+//     {
+//         "name":"dharmendra",
+//        "age":20,
+//        "addhar":30284,
+//        "Time":"10:20",
+//        "person":5
+//     },
+//     {
+//         "name":"dharmendra",
+//        "age":20,
+//        "addhar":30284,
+//        "Time":"10:20",
+//        "person":5
+//     }
+// ]
+// let newob={
+//     name: "pradeep",
+//     age: 25,
+//     addhar: "excellent",
+//     Time:"20:21",
+//     person:6
+
+// }
+// Ardata.push(newob) 
+
+// let ans=Ardata.map((key)=>{return })
