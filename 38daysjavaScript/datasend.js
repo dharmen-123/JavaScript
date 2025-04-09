c3b10d1cedda6ea781f9d9e595a9a3c469fd0ca5
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-04-09 15:49:21 +0530

    update

--- 38daysjavaScript/datasend.js
+++ 38daysjavaScript/datasend.js
@@ -45,11 +45,11 @@ let Upd=async(id)=>{
    let data=await res.json()
    console.log(data);
    let updform = document.querySelector('#updfrom')
+       updform.innerHTML = `
 
-       updform.innerHTML =`
-       Enter Name : <input type="text" id="name" value="${data.name}" > <br><br>
+       Enter Name : <input type="text" id="upname" value="${data.name}" > <br><br>
      Enter Branch: 
-     <select name="" id="branch" value="${data.Branch}">
+     <select name="" id="upbranch" value="${data.Branch}">
         <option value="Select">Select</option>
         <option value="CSE">CSE</option>
         <option value="AIML">AIML</option>
@@ -59,18 +59,45 @@ let Upd=async(id)=>{
         <option value="ME">ME</option>
         <option value="EX">EX</option>
      </select> <br><br>    
-     Enter Course: <input type="text" id="Course" value="${data.Course}"> <br><br>
-     Enter Number: <input type="number" id="number" value="${data.number}"> <br><br>
-     Enter Bus: <input type="text" id="bus" value="${data.Bus}"> <br><br>    
-     Enter College Timing: <input type="time" id="timing" value="${data.Timig}"> <br><br>
-     Enter Admission: <input type="number" id="admission" value="${data.Admmision}"> <br><br>
-     Enter Passout: <input type="number" id="passout" value="${data.Passout}"> <br><br>    
+     Enter Course: <input type="text" id="upCourse" value="${data.Course}"> <br><br>
+     Enter Number: <input type="number" id="upnumber" value="${data.number}"> <br><br>
+     Enter Bus: <input type="text" id="upbus" value="${data.Bus}"> <br><br>    
+     Enter College Timing: <input type="time" id="uptiming" value="${data.Timig}"> <br><br>
+     Enter Admission: <input type="number" id="upadmission" value="${data.Admmision}"> <br><br>
+     Enter Passout: <input type="number" id="uppassout" value="${data.Passout}"> <br><br>    
   
       <input type="submit" value="submit" onclick="return Finalupd("${data.id}")">
    `
 }
 let Finalupd=()=>{
+     
+    let inpname = document.querySelector('#upname').value        
+    let inpcourse = document.querySelector('#upCourse').value
+    let inpbranch = document.querySelector('#upbranch').value
+    let inpbus = document.querySelector('#upbus').value
+    let inptime = document.querySelector('#uptiming').value
+    let inpadmi = document.querySelector('#upadmission').value
+    let inppass = document.querySelector('#uppassout').value
+    let inpnum = document.querySelector('#upnumber').value
 
+      let  url=`http://localhost:3000/College/${id}`
+      fetch(url,{method:"PUT",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify(
+        {
+            "name":inpname,
+            "Course":inpcourse,
+            "Branch":inpbranch,
+            "number":inpnum,
+            "Bus":inpbus,
+            "Timig":inptime,
+            "Admmision":inpadmi,
+            "Passout":inppass
+        })
+      })
+  return false     
 }
 
 let Details=()=>{
